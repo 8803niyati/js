7adb6d75d027213d0e1790f3b0dc478f0fca4815
Niyati Dhanani <niyatidhanani05@gmail.com>
2025-03-26 10:22:27 +0530

    first commit

---
+++ condition.js
@@ -0,0 +1,125 @@
+// condition
+// let x=10;
+// y=20;
+// if(x>y);
+// console.log("y is smaller than x");
+// else{
+//      console.log("x is samller than y");
+// }
+
+// ternarry
+
+// x>y? console.log(" y is smaller no.:"): console.log("x is smaller no.:");
+
+// project 1
+
+// 1.two value find smallest
+// let a=100;
+// b=10;
+
+// if(a<b);
+// console.log("a is smallar than b");
+
+// 2.find positive and negative
+// let a=10;
+// b=-20;
+// c=-30;
+// if(a>b>c);
+//     a>b>c? console.log("a is positive no."):console.log("b is negative no."):console.log("c is nagative no.");
+
+//   3
+// let n;
+// n=18;
+// if(n% 5 == 0){
+//     console.log("the given number is divisiable by 5.");
+// }else{
+//         console.log("the given number is divisiable by 5.");  
+//     }
+
+    // 4
+    // let n;
+    // n=15; 
+    // if( n % 5 == 0 && n% 3 == 0){
+    //     console.log("the given number is divisiable by 5 & 3");
+    //     }else{
+    //             console.log("the given number is divisiable by 5 & 3");  
+    //         }
+    
+//    5
+//   let n;
+//     n=87;   
+//     if( n >= 33.33){
+//         console.log(" the student is passed:");
+//     }else{
+//         console.log(" the student is failed:");
+//     }
+
+// 6.two value find biggest
+// let a=100;
+// b=10;
+
+// if(a<b);
+// console.log("a is biggest than b");,
+
+// project 2
+
+ 5
+// let par=55;
+// if(par>=75){
+//     console.log("grade a:");
+// }else if(par>=60){
+//     console.log("grade b:");
+// }else if(par>=45){
+//     console.log("grade c:");
+// }else if(par>=35){
+//     console.log("grade d:");
+// }else{
+//     console.log("you are failed")
+// }
+
+
+2
+// let age = parseInt(prompt("enter uour age:"));
+// if(age>= 18) {
+//     console.log( " you are eligiable to vote.");
+// }else{
+//     console.log(" you are not  eligiable to vote.");
+// }
+
+6
+// let x = 2500;
+// y=200;
+// if ((x < 2000) || ( x < 3000)){
+//     console.log("value of x:"  + x);
+// }
+// if ((x < 100) ||( x < 500)){
+//     console.log("value of y:" + x);
+// }
+
+4
+// let sleb=4;
+// if(sleb>=0-2500){
+//     console.log("0%");
+// }else if(sleb=2500-5000){
+//     console.log("10%");
+// }else if(sleb>=5000-10000){
+//     console.log("20%");
+// }else if(sleb>=10000 ){
+//     console.log("30%");
+// }else{
+//     console.log("")
+// }
+
+// change password
+
+let oldpass=123,newpass=123,cpass=123;
+if(oldpass==newpass){
+    console.error("old and new password both are same");
+}else{
+    if(newpass==cpass){
+        console.log("password is change");
+    }else{
+        console.log("new pass and confirm pass are not match");
+    }
+}
+
